da487d7a7904e79addea2deba3609c01b205378a
Bharat Gagan <bharatgagan07@gmail.com>
2026-01-01 17:35:24 +0530

    Introduction.py
    Introduction!

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+print("I am Bharat Gagan, Currently pursuing B.Sc in Computer Science from Acharya Narendra Dev College, University of Delhi.")
+print("I have a keen interest in programming and software development.")
+print("I enjoy solving problems and learning new technologies.")
+
+
+print("Thanks Everyone & Keep Supporting Me!")
+print("Happy New Year 2026")
+print("Wishing you all a year filled with success, happiness, and prosperity!")
